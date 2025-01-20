3ff4d6427384ee237e7d8736e6348d2cb370f294
unknown <awan968@aucklanduni.ac.nz>
2025-01-20 23:59:05 +1300

    updated server name to be configurable domain name instead of hardcoding localhost

--- scripts/start-nginx.sh
+++ scripts/start-nginx.sh
@@ -1,2 +1,2 @@
-envsubst '${SERVER_URL} ${CLIENT_URL}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+envsubst '${SERVER_URL} ${CLIENT_URL} ${DOMAIN}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
